cf0fdb48c841641359a857a0a0c3ba68c534ebff
dhsong10 <david.dh.song@gmail.com>
2021-02-20 21:14:22 +0900

    [ADD] form validation

--- chapter02/tacos/src/main/java/sia/tacos/controller/DesignTacoController.java
+++ chapter02/tacos/src/main/java/sia/tacos/controller/DesignTacoController.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +49,29 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(Model model, @Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Dices Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+            );
+    
+            Type[] types = Type.values();
+            for(Type type : types) {
+                model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+            }
+
+            return "design";
+        }
+
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
