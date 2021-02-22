c472519b6ba7d6da939ae06ba6447547c11c9f64
dhsong10 <david.dh.song@gmail.com>
2021-02-22 21:09:51 +0900

    [ADD] IngredientRepository using JdbcTeamplate

--- chapter03/tacos/src/main/java/com/sia/tacos/controller/DesignTacoController.java
+++ chapter03/tacos/src/main/java/com/sia/tacos/controller/DesignTacoController.java
@@ -1,5 +1,6 @@
 package com.sia.tacos.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,7 +10,9 @@ import javax.validation.Valid;
 import com.sia.tacos.domain.Ingredient;
 import com.sia.tacos.domain.Taco;
 import com.sia.tacos.domain.Ingredient.Type;
+import com.sia.tacos.repository.IngredientRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -23,21 +26,18 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping(value = "/design")
 public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
     
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(i -> ingredients.add(i));
 
         Type[] types = Type.values();
         for (Type type : types) {
