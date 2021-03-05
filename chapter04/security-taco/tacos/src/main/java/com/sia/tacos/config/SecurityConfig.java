e252c92870d8f7765b30a727c8fcdd519497c5c5
dhsong10 <david.dh.song@gmail.com>
2021-03-05 22:01:29 +0900

    [ADD] Authentication with UserDetailsService

--- chapter04/security-taco/tacos/src/main/java/com/sia/tacos/config/SecurityConfig.java
+++ chapter04/security-taco/tacos/src/main/java/com/sia/tacos/config/SecurityConfig.java
@@ -1,28 +1,51 @@
 package com.sia.tacos.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/h2-console/**").permitAll().and()
-            .httpBasic();
+                .antMatchers("/h2-console/**").access("permitAll")
+                .antMatchers("/design", "/orders").access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").access("permitAll")
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+            .and()
+                .logout()
+                .logoutSuccessUrl("/")
+            .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+            .and()
+                .headers().frameOptions().sameOrigin();
+    }
 
-        http.csrf().ignoringAntMatchers("/h2-console/**");
-
-        http.headers().frameOptions().sameOrigin();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
 }
